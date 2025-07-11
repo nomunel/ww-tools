a614d2b703c2e5307a98efa7d5b0adb218d44369
nomunel <takashi125f@gmail.com>
2025-07-12 02:24:25 +0900

    OCR修正

--- js/score-checker.js
+++ js/score-checker.js
@@ -1256,7 +1256,7 @@ class OCRWindowController {
         // --- Canvas ---
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
-        const scale = 6;
+        const scale = 8;
         canvas.width = sw * scale;
         canvas.height = sh * scale;
         ctx.filter = `blur(${this.view.blurControl.slider.value}px)`;
